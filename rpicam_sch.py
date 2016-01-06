4fb625e88548b5d08a51c696f8006465c97447a1
istvanzk <istvanzk@gmail.com>
2016-01-06 20:46:30 +0100

    Insert copyright and license text

--- rpicam_sch.py
+++ rpicam_sch.py
@@ -2,11 +2,22 @@
 # -*- coding: utf-8 -*-
 
 """
-Time-lapse with Rasberry Pi controlled camera - VER 2.1 for Python 3.4 (3.5?)
+    Time-lapse with Rasberry Pi controlled camera - VER 2.1 for Python 3.4+
+    Copyright (C) 2016 Istvan Z. Kovacs
 
-Uses APscheduler module to background schedule a daily job and three interval jobs: rpimgdir, rpicam, rpimgdb
-Reads configuration from the //rpiconfig.yaml//
-No other threads are used!
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 TODO: 
 1) Loop over the defined periods per day

c56706c8bdcd87b2117f655060144866f15aaa85
Istvan Z. Kovacs <istvanzk@gmail.com>
2016-01-10 16:45:29 +0100

    Add license text to rpievents

--- rpievents.py
+++ rpievents.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
+"""
+	Time-lapse with Rasberry Pi controlled camera - VER 2.1 for Python 3.4+
+    Copyright (C) 2016 Istvan Z. Kovacs
 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ 
+Implements the Events class (group of events to be used in the rpi threads)
+    
+"""    
 from threading import Event
 from threading import BoundedSemaphore
 

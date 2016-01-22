87f2bd4a4af977c160a7251bd725d1811283c3b6
Istvan Z. Kovacs <istvanzk@gmail.com>
2016-01-22 21:30:23 +0100

    Update description and switch off DEBUG

--- rpicam_sch.py
+++ rpicam_sch.py
@@ -27,7 +27,18 @@ to background schedule three interval jobs:
 					- USB web camera using fswebcam utility 
 	2. rpimgdir:	Manage the set of saved images by rpiCam.  
 	3. rpimgdb:		Manage images in a remote directory (Dropbox SDK, API V2, Python 3.4).
+
 The configuration parameters are read from the rpiconfig.yaml
+
+The image file names are: '%d%m%y-%H%M%S-CAM.jpg', where X is the camera number (ID string). 
+The images are saved locally and remotely in a sub-folder. The sub-folder name is the current date '%d%m%y'
+
+A simple REST request abstraction layer and a light ThingSpeak API SDK is provided in the thingspeak module. 
+The implementation follows to the API documentation at http://community.thingspeak.com/documentation/api/ and 
+the TalkBack API documentation at https://thingspeak.com/docs/talkback. 
+The REST client implementation is based on the official python Xively API client (SDK).
+
+The tool can be launched as an init.d Linux service with the rpicamtest.sh
 
 TODOs: 
 1) Use/enable ThingSpeak TalkBack API
@@ -73,7 +84,7 @@ TSPKTB_ID  = 104
 
 ### Set up the logging
 logging.basicConfig(filename='rpicam.log', filemode='w',
-					level=logging.DEBUG,
+					level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] (%(threadName)-10s) %(message)s',
                     )
 

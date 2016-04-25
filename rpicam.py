4d20c58ba7af329237e623d1a1000ca28f0d3a3e
Istvan Z. Kovacs <istvanzk@gmail.com>
2016-04-25 19:41:39 +0200

    Implement _isDark(). Use the ephem module, sunrise and sunset times, to determine dark hours.

--- rpicam.py
+++ rpicam.py
@@ -379,23 +379,10 @@ class rpiCamClass(rpiBaseClass):
 		'''
 
 		if RPICAM:
-			# Set the current 'dark' time thresholds
-			self._tdark_start = ephem.localtime(self._aal.next_setting(self._sun))
-			self._tdark_stop = ephem.localtime(self._aal.next_rising(self._sun))
-			self._tlocal = datetime.now()
-			
-			#self._tlocal = time.time()
-			#self._tdark_start = time.mktime((self._tlocal.tm_year, self._tlocal.tm_mon, self._tlocal.tm_mday,
-			#			self._config['dark_hours'][0], self._config['dark_mins'][0], 0,
-			#			self._tlocal.tm_wday, self._tlocal.tm_yday, self._tlocal.tm_isdst ))
-			#self._tdark_stop = time.mktime((self._tlocal.tm_year, self._tlocal.tm_mon, self._tlocal.tm_mday,
-			#			self._config['dark_hours'][1], self._config['dark_mins'][1], 0,
-			#			self._tlocal.tm_wday, self._tlocal.tm_yday, self._tlocal.tm_isdst ))
-
+		
 			# Set the "dark" exposure parameters when needed
-			#if (self._tlocal >= self._tdark_start) or (self._tlocal <= self._tdark_stop):
-			if (self._tlocal >= self._tdark_start) and (self._tlocal <= self._tdark_stop):
-
+			if self._isDark():
+			
 				if self.camid == 'CAM1':
 					self._camera.awb_mode = 'auto'
 					self._camera.iso = 800
@@ -452,6 +439,33 @@ class rpiCamClass(rpiBaseClass):
 		#elif RASPISTILL:
 			# TODO!
 
+	def _isDark(self):
+		'''
+		Determine if current time is in the "dark" period.
+		'''
+
+		# Set the current 'dark' time thresholds
+		self._tdark_start = ephem.localtime(self._aal.previous_setting(self._sun))
+		self._tdark_stop = ephem.localtime(self._aal.previous_rising(self._sun))
+		
+		if (self._tdark_start > self._tdark_stop):
+			return True
+		else:
+			return False	
+		
+		#self._tlocal = time.time()
+		#self._tdark_start = time.mktime((self._tlocal.tm_year, self._tlocal.tm_mon, self._tlocal.tm_mday,
+		#			self._config['dark_hours'][0], self._config['dark_mins'][0], 0,
+		#			self._tlocal.tm_wday, self._tlocal.tm_yday, self._tlocal.tm_isdst ))
+		#self._tdark_stop = time.mktime((self._tlocal.tm_year, self._tlocal.tm_mon, self._tlocal.tm_mday,
+		#			self._config['dark_hours'][1], self._config['dark_mins'][1], 0,
+		#			self._tlocal.tm_wday, self._tlocal.tm_yday, self._tlocal.tm_isdst ))
+
+		#if (self._tlocal >= self._tdark_start) or (self._tlocal <= self._tdark_stop):
+		#:
+					
+		
+	
 	def _switchIR(self, bONOFF):
 		'''
 		Switch ON/OFF the IR lights

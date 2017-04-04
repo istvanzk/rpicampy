cdb112f2479c6bbcd48f3cba852847a7335d148e
Istvan Z. Kovacs <istvanzk@gmail.com>
2017-04-04 21:48:45 +0200

    Rename and update daemon name

--- rpicamsch.sh
+++ rpicamsch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ### BEGIN INIT INFO
-# Provides:          RPiCamTest
+# Provides:          RPiCamSch
 # Required-Start:    $remote_fs $syslog
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
@@ -18,7 +18,7 @@
 # Change the next 3 lines to suit where you install your script and what you want to call it
 DIR=/usr/local/bin/rpicam
 DAEMON=rpicam_sch.py
-DAEMON_NAME=rpicamtest
+DAEMON_NAME=rpicamsch
 
 # This next line determines what user the script runs as.
 # Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python

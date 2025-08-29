3e6dda43b2915ecd897c1f6ccb3930c5edded92b
Istvan Z. Kovacs <istvanzk@gmail.com>
2025-08-29 20:50:07 +0200

    Increase analog gain w/o IRL

--- camdev.py
+++ camdev.py
@@ -96,7 +96,7 @@ def _setCamExp(is_dark: bool, use_irl: bool):
                 "ExposureTime": 120000, #usec
                 "Contrast": 5, # Floating point number from 0.0 to 32.0
                 "Brightness": 0.4, # Floating point number from -1.0 to 1.0
-                "AnalogueGain": 4.0,
+                "AnalogueGain": 8.0,
                 "AwbEnable": True, 
                 "AwbMode": controls.AwbModeEnum.Auto,
                 #"FrameDurationLimits": (2000000,2000000), #usec

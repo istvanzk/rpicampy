7ccc60bc4e9f0899b0b14ebde6f4d7982e787958
Istvan Z. Kovacs <istvanzk@gmail.com>
2026-09-06 00:24:17 +0200

    Fix time of first error

--- rpicam.py
+++ rpicam.py
@@ -670,13 +670,16 @@ class rpiCamClass(rpiBaseClass):
         _time = self.errorTime
         _delay = self.errorDelay
         _count = self.errorCount
+        _time_first = 0
         if _level == ERRNONE:
             return
         elif _level == ERRLEV0: 
             return
         elif _level == ERRLEV1: 
-            # Timeout error (jobRun Process timeout, see _run() method)
-            if (time.time() - _time) >= _delay:
+            # Timeout error (jobRun timeout, jobRun Process timeout, see _run() method)
+            if _time_first == 0:
+                _time_first=time.time()
+            if (time.time() - _time_first) >= _delay:
                 # The previous job execution parameters
                 tstart_per, tstop_per, tinterval_per = self.timePeriodIntv
                 rpiLogger.info("rpicam:: handleJobExecuted(): ERRLEV1 (timeout): Grace period %d seconds has passed. Job will be rescheduled with increased run interval to %.1f seconds.", _delay, INTERVAL_INCREASE_FACTOR * tinterval_per)
